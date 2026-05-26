cd99bb581df75d046a2e9e3acc660ecd50dc8acb
Sawant Kumar <sawantkumarsawant6014@gmail.com>
2026-05-26 17:43:59 +0530

    fixed login password hashing

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -5,14 +5,30 @@ from rest_framework import serializers
 class SignupSerializer(serializers.ModelSerializer):
 
     class Meta:
+
         model = User
-        fields = ['username', 'email', 'password']
+
+        fields = [
+            'username',
+            'email',
+            'password'
+        ]
+
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
 
     def create(self, validated_data):
 
         user = User.objects.create_user(
+
             username=validated_data['username'],
+
             email=validated_data['email'],
+
             password=validated_data['password']
         )
 
